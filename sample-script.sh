b190da5cc4b633f14c658471e93ec59f15aa45e5
Mohiul Islam Miraz <mohiulislam23@gmail.com>
2025-05-09 22:45:44 +0600

    Sample script

---
+++ sample-script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#########################
+# Author: Miraz
+# Date: 07-05-2025
+# Version: v1
+# About: This script will create a folder and two files
+# ######################
+
+#Create a folder
+mkdir samplefolder
+
+#Create two files
+cd samplefolder
+touch samplefile1 samplefile2
