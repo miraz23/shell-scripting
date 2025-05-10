94b658d5a3b76c60381c58ac2a120a460eff47a3
Mohiul Islam Miraz <mohiulislam23@gmail.com>
2025-05-10 12:23:30 +0600

    If else in bash

---
+++ if-else.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Author: Miraz
+# Date: 08-05-2025
+# Description: This script demonstrates if-else in bash
+
+a=2
+b=3
+
+if [ "$a" -gt "$b" ]; then
+    echo "a is greater than b"
+else
+    echo "b is greater than a"
+fi
